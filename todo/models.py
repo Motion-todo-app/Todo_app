f84f3f54c808162f474abbb5fe724c34683c79d6
Eugene <eugenereybulahan@gmail.com>
2022-11-16 22:00:07 +0800

    feat: BE: TODO-41 List of task and categories only

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _  # renaming variables
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_('Category Name'), max_length=200,
+                            blank=False,
+                            null=True,
+                            help_text='Add categories to your task.')
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class Task(models.Model):
+    name = models.CharField(_('What\'s to be done'), max_length=250,
+                            blank=False,
+                            null=True,
+                            help_text='Add your todo task.')
+    date = models.DateField(_('Pick a date'),
+                            null=True,
+                            blank=False,
+                            help_text='Click the box and select date.')
+    time = models.DateField(_('Pick a time'),
+                            null=True,
+                            blank=False,
+                            help_text='Click the box and select time.')
+    category = models.ForeignKey(
+                                 Category,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=False,
+                                 help_text='Click the dropdown and select your category.')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todo'
--- todo_app/settings.py
+++ todo_app/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-3qn_o-xz_%)cbd@934aae3$t&&-bap1lplc6g1x3(o=+=%$=^f'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1"  # 1 == True
-
+#DEBUG = str(os.environ.get('DEBUG')) == "1"  # 1 == True
+DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     # myapp
     'authentication',
     'todo',
+    'rest_framework',
     # third party
     'django_filters',
 
@@ -77,9 +78,9 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -114,8 +115,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
+
+# ManifestStaticFilesStorage is recommended in production, to prevent outdated
+# JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
+# See https://docs.djangoproject.com/en/4.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
