9a868ac400be22ae7b29c756a800fbc613381482
kolaoladejo <97963737+kolaoladejo@users.noreply.github.com>
2022-11-20 23:49:35 -0800

    user unit testing

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import User
+from django.conf import settings
 
 # Create your tests here.
+class UserManagerTestCase(TestCase):
+    def test_main_create_user(self):
+        user = CustomUser.objects.create_user('jdoe@gmail.com', 'password123')
+        self.assertTrue(isinstance(user, User))
